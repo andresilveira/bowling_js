1df95ab5e162eedc0122f751cc32183c2c7327fb
andresilveira <andresilveirah@gmail.com>
2015-08-12 12:40:17 +0200

    add Player

--- src/Player.js
+++ src/Player.js
@@ -1,2 +1,15 @@
-function Player() {
+'use strict'
+
+function Player(id, name) {
+  if( typeof id == 'undefined' ){
+    throw TypeError('a Player requires an id');
+  }
+  
+  this.id = id;
+  this.name = name || 'Unknown';
+  this.score = 0;
+}
+
+User.prototype = {
+  constructor: User
 }

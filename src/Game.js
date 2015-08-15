ddaca75860bdc8b79a57419d2ce0f57f7609939c
andresilveira <andresilveirah@gmail.com>
2015-08-15 20:21:22 +0200

    add html prototype

--- src/Game.js
+++ src/Game.js
@@ -8,7 +8,7 @@ function Game(players) {
 
 Game.prototype = {
   constructor: Game,
-  _add_player: function(player_or_players){ this.players.add(player_or_players); },
-  add_player: function(player){ this._add_player(player); },
-  add_players: function(players){ this._add_player(players); }
+  add_player: function(player){ 
+    this.players.add(player);
+  }
 };
